5068d5384768144daadd615023efbf77059e7794
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-02-23 10:33:06 +1300

    Dj1.8: repl longerusername with custom user model
    The longerusername package is not really compatible with Django 1.7+ - it only
    hooks into South migrations, and Django 1.7+ does not allow the "monkeypatch"
    approach that longerusername uses to modify the Auth.User model.
    
    Instead, do the proper thing and define a custom user model as per
    https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#specifying-a-custom-user-model
    
    To make the transition easy:
    * Create a model that inherits from Auth.AbstractUser, only modifying the username length
    * Make the model use the auth_user DB table so it picks up existing data
    * Call the model User so that links from other existing tables still work
    
    For existing Django 1.4 databases, the model would just inherit the existing table structure.
    
    For new Django 1.8 databases, the generated initial migrations would create the tables the same way.

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from edumanage.models import Institution
+import re
 
+
+class User(AbstractUser):
+    class Meta:
+        db_table = 'auth_user';
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+User._meta.get_field('username').max_length = 255
+# try also updating the help_text
+try:
+    help_text = User._meta.get_field('username').help_text._proxy____args[0]
+    if help_text != None:
+	User._meta.get_field('username').help_text = _(re.sub("[0-9]+ characters", "255 characters", help_text))
+except:
+    pass
+
+admin.site.register(User, UserAdmin)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
