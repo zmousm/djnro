47c3ba4ba86f2f1d8951bc3d339fa679be4a839e
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-03-07 14:12:25 +1300

    Django 1.8: mark strings for translation
    Add translation support for messages in edumanage/fields.py as requested in comments to 77f403e

--- edumanage/fields.py
+++ edumanage/fields.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext as _
 from django.core.validators import EmailValidator
 
 
@@ -9,14 +10,14 @@ class MultipleEmailsField(forms.Field):
         and normalizes the data to a list of the email strings.
         """
         if not value:
-            raise forms.ValidationError('Enter at least one e-mail address. Multiple email addresses Should be separated with semicolon (;)')
+            raise forms.ValidationError(_('Enter at least one e-mail address. Multiple email addresses Should be separated with semicolon (;)'))
         emails = value.split(';')
         for email in emails:
             # Create our own instance with
             # * blank whitelist - no '@localhost' permitted
             # * custom error message (referring to the email address being verified in question)
             # A failed validation will raise a ValidationError - no need to check return value
-            EmailValidator(whitelist=[],message= '%s is not a valid e-mail address.' % email)(email)
+            EmailValidator(whitelist=[],message= _('%s is not a valid e-mail address.') % email)(email)
 
         # Always return the cleaned data.
         return ';'.join(emails)
